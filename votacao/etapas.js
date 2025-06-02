66099fc005217812ed4a6ef89df6f27681e397e9
Jean Torres <88167764+jeantorres1697@users.noreply.github.com>
2025-06-02 19:19:12 -0300

    Update etapas.js

--- votacao/etapas.js
+++ votacao/etapas.js
@@ -8,7 +8,7 @@ let etapas = [
                     name: 'Michael Jackson',
                     partido: 'Rusber',
                     fotos: [
-                        {url:'/images/michael.png', legenda: 'Vereador'}
+                        {url:'michael.png', legenda: 'Vereador'}
                     ], 
                     votos: 0
                 },
